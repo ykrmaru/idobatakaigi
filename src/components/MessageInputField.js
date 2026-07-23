7113ab62b08d2a00e2955b321a91039c7a1a760b
Yukari Maruyama <ykrmaruyama0323@gmail.com>
2026-07-23 09:39:45 +0900

    input field design

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: "2",
+  },
+});
+
 const MessageInputField = ({ name }) => {
-  return <div>MessageInputField</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageInputField: {name}</div>;
 };
 
 export default MessageInputField;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: "1",
+  },
+});
+
 const MessageList = ({ name }) => {
-  return <div>MessageList: {name}</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList: {name}</div>;
 };
 
 export default MessageList;
