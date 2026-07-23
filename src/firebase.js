a520175314b6be7eed3d93c6d6317a09c0d0d806
Yukari Maruyama <ykrmaruyama0323@gmail.com>
2026-07-23 14:23:16 +0900

    remove hard corded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,21 @@
 import firebase from "firebase";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdYjjkTDK_NQy1qBA4DT6gkL3aP__NaC4",
-  authDomain: "idobatakaigi-with-ham-ff270.firebaseapp.com",
-  projectId: "idobatakaigi-with-ham-ff270",
-  storageBucket: "idobatakaigi-with-ham-ff270.firebasestorage.app",
-  messagingSenderId: "834662640120",
-  appId: "1:834662640120:web:da13f29da4c47e3e6f8e0d",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
