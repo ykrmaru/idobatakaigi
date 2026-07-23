2c9edb2d2858b9eacc2aa17e1d38f2d2a7417783
Yukari Maruyama <ykrmaruyama0323@gmail.com>
2026-07-23 09:50:54 +0900

    input field grid

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,16 +1,28 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     gridRow: "2",
+    margin: "24px",
   },
 });
 
 const MessageInputField = ({ name }) => {
   const classes = useStyles();
-  return <div className={classes.root}>MessageInputField: {name}</div>;
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default MessageInputField;
